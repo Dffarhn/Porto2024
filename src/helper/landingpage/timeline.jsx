1d5c180d4b424bb5e55a9e336551dfa09705203f
Muhammad Daffa Raihan <d.raihan2004@gmail.com>
2024-08-26 15:51:34 +0700

    make sure the image on public

--- src/helper/landingpage/timeline.jsx
+++ src/helper/landingpage/timeline.jsx
@@ -14,25 +14,27 @@ export default function TimelineContainer() {
     {
       title: "Phase 1",
       content: `Di SMA kelas 11, saya pertama kali diperkenalkan dengan dunia programming—sebuah dunia yang penuh tantangan dan peluang. Dengan semangat yang tinggi, saya menulis kode pertama saya dalam Python menggunakan perintah sederhana, print("hello world"). Momen ini membuka pintu bagi perjalanan panjang saya dalam pengembangan perangkat lunak.`,
-      images: ["src/assets/journey/python.png", "src/assets/journey/vscode.png"],
+      images: ["/journey/python.png", "/journey/vscode.png"],
     },
     {
       title: "Phase 2",
-      content: "Saat minat saya pada programming berkembang, saya menyadari bahwa ini lebih dari sekadar hobi—ini adalah passion saya. Dengan tekad yang kuat, saya melanjutkan studi di jurusan Informatika di Universitas Islam Indonesia untuk memperdalam ilmu saya.",
-      images: ["src/assets/journey/Figma.png", "src/assets/journey/uii.webp"],
+      content:
+        "Saat minat saya pada programming berkembang, saya menyadari bahwa ini lebih dari sekadar hobi—ini adalah passion saya. Dengan tekad yang kuat, saya melanjutkan studi di jurusan Informatika di Universitas Islam Indonesia untuk memperdalam ilmu saya.",
+      images: ["/journey/Figma.png", "/journey/uii.webp"],
     },
     {
       title: "Phase 3",
-      content: "Pada semester 3 kuliah, saya mulai memfokuskan minat saya pada bidang backend engineering. Kecintaan saya terhadap algoritma dan struktur data membawa saya untuk mulai membuat RESTful API sebagai langkah awal dalam perjalanan profesional saya.",
-      images: ["src/assets/journey/java.png", "src/assets/journey/php.png", "src/assets/journey/express.png", "src/assets/journey/mysql.svg"],
+      content:
+        "Pada semester 3 kuliah, saya mulai memfokuskan minat saya pada bidang backend engineering. Kecintaan saya terhadap algoritma dan struktur data membawa saya untuk mulai membuat RESTful API sebagai langkah awal dalam perjalanan profesional saya.",
+      images: ["/journey/java.png", "/journey/php.png", "/journey/express.png", "/journey/mysql.svg"],
     },
     {
       title: "Phase 4",
       content: "Saat ini, saya terus mengembangkan pengetahuan saya dengan mempelajari framework yang lebih terstruktur dan mengeksplorasi peran frontend engineering, memperluas keahlian saya dalam pengembangan aplikasi secara menyeluruh.",
-      images: ["src/assets/journey/nestjs.png", "src/assets/journey/postgresql.png", "src/assets/journey/astro.png", "src/assets/journey/tailwind.webp"],
+      images: ["/journey/nestjs.png", "/journey/postgresql.png", "/journey/astro.png", "/journey/tailwind.webp"],
     },
   ];
-  
+
   return (
     <Timeline
       position={isLaptop ? "alternate-reverse" : "right"}
